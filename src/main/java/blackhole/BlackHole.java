acaf5d6994912fc6af513cc71bbcb6328ef4e5c3
PrototypeTrousers <salomaoo@gmail.com>
2023-07-18 16:46:40 -0300

    initial commit

---
+++ src/main/java/blackhole/BlackHole.java
@@ -0,0 +1,38 @@
+package blackhole;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Tags.MODID, version = Tags.VERSION, name = Tags.MODNAME, acceptedMinecraftVersions = "[1.12.2]")
+public class BlackHole {
+    public static final Logger LOGGER = LogManager.getLogger(Tags.MODID);
+
+    @Mod.EventHandler
+    // preInit "Run before anything else. Read your config, create blocks, items, etc. (Remove if not needed)
+    public void preInit(FMLPreInitializationEvent event) {
+        // register to the event bus so that we can listen to events
+        MinecraftForge.EVENT_BUS.register(this);
+        LOGGER.info("I am " + Tags.MODNAME + " + at version " + Tags.VERSION);
+    }
+
+    @Mod.EventHandler
+    // load "Do your mod setup. Build whatever data structures you care about." (Remove if not needed)
+    public void init(FMLInitializationEvent event) {
+    }
+
+    @Mod.EventHandler
+    // postInit "Handle interaction with other mods, complete your setup based on this." (Remove if not needed)
+    public void postInit(FMLPostInitializationEvent event) {
+    }
+
+    @Mod.EventHandler
+    // register server commands in this event handler (Remove if not needed)
+    public void serverStarting(FMLServerStartingEvent event) {
+    }
+}
